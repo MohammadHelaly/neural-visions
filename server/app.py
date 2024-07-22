6566716f81fdf5554d95c27151fff9dc52224804
MohammadHelaly <mohammad.helaly@gmail.com>
2024-07-22 19:51:50 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -3,15 +3,15 @@ import torch
 import pickle
 import os
 import urllib.request
+import uuid
 from model import LinearNet
 
 app = Flask(__name__)
 
 path_current_directory = os.path.dirname(os.path.realpath(__file__))
-path_model_encoder_answer = os.path.join(path_current_directory, './saved/model_encoder_answer.pkl')
-path_model_encoder_answer_type = os.path.join(path_current_directory, './saved/model_encoder_answer_type.pkl')
-path_model = os.path.join(path_current_directory, './saved/VisualQnA.pth')
-path_image_user = os.path.join(path_current_directory, './images/image_user.jpg')
+path_model_encoder_answer = os.path.join(path_current_directory, './saved/encoders/model_encoder_answer.pkl')
+path_model_encoder_answer_type = os.path.join(path_current_directory, './saved/encoders/model_encoder_answer_type.pkl')
+path_model = os.path.join(path_current_directory, './saved/models/VisualQnA.pth')
 
 with open(path_model_encoder_answer, 'rb') as f:
     model_encoder_answer = pickle.load(f)
@@ -23,47 +23,48 @@ model = LinearNet(num_classes=5410, device = torch.device("cpu"), model = "ViT-L
 model.load_model(path_model)
 
 def allowed_format(image_name):
-
     allowed_extensions = {'png','jpg','jpeg'}
     return '.' in image_name and image_name.rsplit('.',1)[1].lower() in allowed_extensions
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    image_url_user = request.form.get('image_url')
+    question_user = request.form.get('question')
+    unique_image_filename = str(uuid.uuid4())
+    path_image_user = os.path.join(path_current_directory, './images/'+ unique_image_filename +'.jpg')
 
-    if request.method == 'POST':
-        image_url_user = request.form.get('image_url')
-        question_user = request.form.get('question')
-
-        if question_user is None or question_user == "":
-            return jsonify({'error':'question not provided'})
+    if question_user is None or question_user == "":
+        return jsonify({'error':'question not provided'})
         
-        if 'image' in request.files:
-            image_user = request.files['image']
+    if 'image' in request.files:
+        image_user = request.files['image']
 
-            if not allowed_format(image_user.filename):
-                return jsonify({'error':'image format not supported'})
+        if not allowed_format(image_user.filename):
+            return jsonify({'error':'image format not supported'})
             
-            else:
-                image_user.save(path_image_user)
-
-        elif image_url_user:
-            urllib.request.urlretrieve(image_url_user, path_image_user)
-
         else:
-            return jsonify({'error':'image not provided'})
+            image_user.save(path_image_user)
 
-        try:
-            predicted_answer, predicted_answer_type, answerability = model.test_model(image_path = path_image_user, question = question_user)
-            answer = model_encoder_answer.inverse_transform(predicted_answer.cpu().detach().numpy())
-            answer_type = model_encoder_answer_type.inverse_transform(predicted_answer_type.cpu().detach().numpy())
-        
-            response = jsonify({'answer': answer[0][0], 'answer_type': answer_type[0][0], 'answerability': answerability.item()})
-            response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
-            response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-            response.headers.add('Access-Control-Allow-Methods', 'POST')
-            return response
-        except:
-            return jsonify({'error':'error during prediction'}) 
+    elif image_url_user:
+        urllib.request.urlretrieve(image_url_user, path_image_user)
+
+    else:
+        return jsonify({'error':'image not provided'})
+
+    try:
+        predicted_answer, predicted_answer_type, answerability = model.test_model(image_path = path_image_user, question = question_user)
+        answer = model_encoder_answer.inverse_transform(predicted_answer.cpu().detach().numpy())
+        answer_type = model_encoder_answer_type.inverse_transform(predicted_answer_type.cpu().detach().numpy())
+
+        os.remove(path_image_user) 
+
+        response = jsonify({'answer': answer[0][0], 'answer_type': answer_type[0][0], 'answerability': answerability.item()})
+        response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        response.headers.add('Access-Control-Allow-Methods', 'POST')
+        return response
+    except:
+        return jsonify({'error':'error during prediction'}) 
 
 if __name__ == '__main__':
     app.run(debug=True)
