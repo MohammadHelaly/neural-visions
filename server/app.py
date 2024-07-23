770c114b3c9f71ecf3dd8f245b68b4d46bcf2350
MohammadHelaly <mohammad.helaly@gmail.com>
2024-07-23 19:49:00 +0300

    Serve frontend from server

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, send_from_directory, request, jsonify
 import torch
 import pickle
 import os
@@ -6,15 +6,14 @@ import urllib.request
 import uuid
 from model import LinearNet
 
-app = Flask(__name__)
-
 path_current_directory = os.path.dirname(os.path.realpath(__file__))
+path_build_directory = os.path.join(path_current_directory, '../client/build')
 path_images_directory = os.path.join(path_current_directory, './images')
 path_model_encoder_answer = os.path.join(path_current_directory, './saved/encoders/model_encoder_answer.pkl')
 path_model_encoder_answer_type = os.path.join(path_current_directory, './saved/encoders/model_encoder_answer_type.pkl')
 path_model = os.path.join(path_current_directory, './saved/models/VisualQnA.pth')
 
-os.makedirs(path_images_directory, exist_ok=True)
+os.makedirs(path_images_directory, exist_ok = True)
 
 with open(path_model_encoder_answer, 'rb') as f:
     model_encoder_answer = pickle.load(f)
@@ -22,28 +21,34 @@ with open(path_model_encoder_answer, 'rb') as f:
 with open(path_model_encoder_answer_type, 'rb') as f:
     model_encoder_answer_type = pickle.load(f)
 
-model = LinearNet(num_classes=5410, device = torch.device("cpu"), model = "ViT-L/14@336px").to(torch.device("cpu"))
+model = LinearNet(num_classes = 5410, device = torch.device("cpu"), model = "ViT-L/14@336px").to(torch.device("cpu"))
 model.load_model(path_model)
 
 def allowed_format(image_name):
     allowed_extensions = {'png','jpg','jpeg'}
     return '.' in image_name and image_name.rsplit('.',1)[1].lower() in allowed_extensions
 
-@app.route('/predict', methods=['POST'])
+app = Flask(__name__, static_folder = path_build_directory, static_url_path = '/')
+
+@app.route('/', methods = ['GET'])
+def home():
+    return send_from_directory(path_build_directory, 'index.html')  
+
+@app.route('/predict', methods = ['POST'])
 def predict():
     image_url_user = request.form.get('image_url')
     question_user = request.form.get('question')
     unique_image_filename = str(uuid.uuid4())
-    path_image_user = os.path.join(path_images_directory, unique_image_filename +'.jpg')
+    path_image_user = os.path.join(path_images_directory, unique_image_filename + '.jpg')
 
     if question_user is None or question_user == "":
-        return jsonify({'error':'question not provided'})
+        return jsonify({'error': 'question not provided'})
         
     if 'image' in request.files:
         image_user = request.files['image']
 
         if not allowed_format(image_user.filename):
-            return jsonify({'error':'image format not supported'})
+            return jsonify({'error': 'image format not supported'})
             
         else:
             image_user.save(path_image_user)
@@ -52,7 +57,7 @@ def predict():
         urllib.request.urlretrieve(image_url_user, path_image_user)
 
     else:
-        return jsonify({'error':'image not provided'})
+        return jsonify({'error': 'image not provided'})
 
     try:
         predicted_answer, predicted_answer_type, answerability = model.test_model(image_path = path_image_user, question = question_user)
@@ -65,10 +70,11 @@ def predict():
         response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
         response.headers.add('Access-Control-Allow-Methods', 'POST')
+        
         return response
     except:
-        return jsonify({'error':'error during prediction'}) 
+        return jsonify({'error': 'error during prediction'}) 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
        
