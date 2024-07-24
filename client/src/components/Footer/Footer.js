cfdb3324b9d9ed291c8eda36a3b89f837a34905c
MohammadHelaly <mohammad.helaly@gmail.com>
2024-07-24 23:51:17 +0300

    Update footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -11,7 +11,71 @@ const Footer = () => {
 			<div
 				ref={elementRef}
 				className={`container text-light ${styles["footer-container"]}`}>
-				<Links />
+				<div className={styles["footer-content"]}>
+					<div>
+						<p className={styles["copyright"]}>
+							Background by{" "}
+							<a
+								href="https://objkt.com/@xponential"
+								target="_blank"
+								rel="noreferrer">
+								xponentialdesign
+							</a>
+							.
+						</p>
+						<p className={styles["copyright"]}>
+							Neural network deep learning model developed with{" "}
+							<a
+								href="https://pytorch.org/"
+								target="_blank"
+								rel="noreferrer">
+								PyTorch
+							</a>
+							.
+						</p>
+						<p className={styles["copyright"]}>
+							Deep learning model encorporated with the{" "}
+							<a
+								href="https://openai.com/research/clip/"
+								target="_blank"
+								rel="noreferrer">
+								OpenAI CLIP encoder
+							</a>
+							.
+						</p>
+						<p className={styles["copyright"]}>
+							Model trained on the{" "}
+							<a
+								href="https://vizwiz.org/tasks-and-datasets/vqa/"
+								target="_blank"
+								rel="noreferrer">
+								VizWiz VQA dataset
+							</a>
+							.
+						</p>
+						<p className={styles["copyright"]}>
+							Backend server developed with{" "}
+							<a
+								href="https://flask.palletsprojects.com/"
+								target="_blank"
+								rel="noreferrer">
+								Flask
+							</a>
+							.
+						</p>
+						<p className={styles["copyright"]}>
+							Developed with{" "}
+							<a
+								href="https://reactjs.org/"
+								target="_blank"
+								rel="noreferrer">
+								React.js
+							</a>
+							.
+						</p>
+					</div>
+					<Links />
+				</div>
 				<hr />
 				<p className={styles["copyright"]}>&copy; {currentYear}</p>
 			</div>
