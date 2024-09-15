4a7d73bd2cc8d5e284ee22991b2fff75da1d6708
MohammadHelaly <mohammad.helaly@gmail.com>
2024-09-15 22:46:20 +0300

    Update scrolling

--- client/src/components/nav-links/nav-links.tsx
+++ client/src/components/nav-links/nav-links.tsx
@@ -1,16 +1,23 @@
+import { scrollTo } from "@/lib/utils";
+
 const links = ["Try VQnA", "About", "Contact"];
 
 const NavLinks = () => {
   return links.map((link) => {
-    const href = "#" + (link.toLowerCase().split(" ")[1] ?? link.toLowerCase());
+    const handleClick = () => {
+      scrollTo(link.toLowerCase().split(" ")[1] ?? link.toLowerCase());
+    };
 
     return (
       <li key={link}>
-        <a href={href} className="flex h-full items-center px-2 py-1">
+        <span
+          onClick={handleClick}
+          className="flex h-full cursor-pointer items-center px-2 py-1"
+        >
           <p className="w-full text-center font-poppins text-base font-normal text-white">
             {link}
           </p>
-        </a>
+        </span>
       </li>
     );
   });
